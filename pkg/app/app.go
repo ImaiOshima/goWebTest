83661b9ae3db082f012369e4e5b9b69996f28c1d
DESKTOP-CM9A6O1\61635 <616357796@qq.com>
2022-08-31 00:07:11 +0800

    update

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,6 +1,10 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"goWebTest/pkg/errcode"
+	"net/http"
+)
 
 type Response struct {
 	Ctx *gin.Context
@@ -14,4 +18,31 @@ type Pager struct {
 
 func NewResponse(ctx *gin.Context) *Response {
 	return &Response{Ctx: ctx}
+}
+
+func (r *Response) ToResponse(data interface{}) {
+	if data == nil {
+		data = gin.H{}
+	}
+	r.Ctx.JSON(http.StatusOK, data)
+}
+
+func (r *Response) ToResponseList(list interface{}, totalRows int) {
+	r.Ctx.JSON(http.StatusOK, gin.H{
+		"list": list,
+		"pager": Pager{
+			Page:      GetPage(r.Ctx),
+			PageSize:  GetPageSize(r.Ctx),
+			TotalRows: totalRows,
+		},
+	})
+}
+
+func (r *Response) ToErrorResponse(err *errcode.Error) {
+	response := gin.H{"code": err.Code(), "msg": err.Msg()}
+	details := err.Details()
+	if len(details) > 0 {
+		response["details"] = details
+	}
+	r.Ctx.JSON(err.StatusCode(), response)
 }
